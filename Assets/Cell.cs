eb79806adf82c3d7d74cd80ae071fa6563466671
Mike Odom <5105729+mike-odom@users.noreply.github.com>
2019-05-18 12:04:03 -0700

    Balls balls balls!

---
+++ Assets/Cell.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public Material level1Material;
+    public Material level2Material;
+    public Material level3Material;
+
+    private List<GameObject> touching = new List<GameObject>();
+
+    private int level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1)
+        {
+            NucleusController.instance.RemoveCell(this);
+        }
+
+        if (touching.Count > 2)
+        {
+            var pos = (transform.position + touching[0].transform.position + touching[1].transform.position) / 3;
+
+            var nc = NucleusController.instance;
+
+            var newCell = nc.AddCell();
+            newCell.transform.position = pos;
+            newCell.SetLevel(level + 1);
+
+            nc.RemoveCell(touching[0].GetComponent<Cell>());
+            nc.RemoveCell(touching[1].GetComponent<Cell>());
+            nc.RemoveCell(this.GetComponent<Cell>());
+
+            touching.RemoveRange(0, 2);
+           
+        }
+    }
+
+    void OnDestroy()
+    {
+        foreach (var touched in touching)
+        {
+            var cell = touched.GetComponent<Cell>();
+            if (cell != null)
+            {
+                cell.OtherCellDestroyed(this.gameObject);
+            }
+        }
+    }
+
+    public void OtherCellDestroyed(GameObject cellObj)
+    {
+        touching.Remove(cellObj);
+    }
+
+    public void SetLevel(int newLevel)
+    {
+        level = newLevel;
+
+        Material material = null;
+
+        if (level == 1)
+        {
+            material = level1Material;
+        } else if (level == 2)
+        {
+            Debug.Log("Setting level 2");
+            material = level2Material;
+        }else if (level == 3)
+        {
+            Debug.Log("Setting level 3");
+            material = level3Material;
+        }
+
+        var renderer = gameObject.GetComponent<MeshRenderer>();
+        var materials = renderer.materials;
+        materials[0] = material;
+        renderer.materials = materials;
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        var cell = col.gameObject.GetComponent<Cell>();
+        if (!cell) return;
+
+        if (cell.level == level)
+        {
+            touching.Add(col.gameObject);
+        }
+    }
+
+    public void OnCollisionLeave(Collision col)
+    {
+        touching.Remove(col.gameObject);
+    }
+}
---
+++ Assets/NucleusController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NucleusController : MonoBehaviour {
+    public static NucleusController instance { get; private set; }
+
+    public GameObject spawnPoint;
+    public GameObject cellPrefab;
+
+    private List<Cell> cellList = new List<Cell>();
+
+    private float lastSpawnTime;
+
+    void Awake() {
+        instance = this;
+        cellPrefab.SetActive(false);
+    }
+
+    void Start()
+    {
+        lastSpawnTime = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > lastSpawnTime + 1)
+        {
+            //AddCell();
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    public void AddCellButtonClicked()
+    {
+        AddCell();
+    }
+
+    public Cell AddCell()
+    {
+        GameObject cellObj = Instantiate(cellPrefab);
+        cellObj.SetActive(true);
+        cellObj.transform.position =
+            spawnPoint.transform.position + new Vector3(Random.Range(-4, 4), 0, Random.Range(-4, 4));
+
+        Cell cell = cellObj.GetComponent<Cell>();
+
+        cellList.Add(cell);
+
+        cell.SetLevel(1);
+
+        return cell;
+    }
+
+    public void RemoveCell(Cell cell)
+    {
+        cellList.Remove(cell);
+        Destroy(cell.gameObject);
+    }
+
+    const float G = 5f; // universal gravity constant
+    void FixedUpdate()
+    {
+        if (cellList.Count < 2) return;
+
+        // calculate only other planets gravity!
+        foreach (var cell in cellList) {
+            var resForce = Vector3.zero;
+            var cellRb = cell.GetComponent<Rigidbody>();
+            foreach (var testCell in cellList)
+            {
+                if (testCell == cell) continue;
+
+                var testRb = testCell.GetComponent<Rigidbody>();
+
+                var dir = testCell.gameObject.transform.position - cell.transform.position; // get the force direction
+                dir.y = 0;
+                var dist2 = dir.sqrMagnitude; // get the squared distance
+
+                // calculate the force intensity using Newton's law
+                var gForce = G * cellRb.mass * testRb.mass / dist2;
+                gForce = Math.Min(gForce, 1);
+
+                resForce += gForce * dir.normalized; // accumulate in the resulting force variable
+            }
+            
+            cellRb.AddForce(resForce); // apply the resulting gravity force
+        }
+      
+    }
+
+    void MurderCluster()
+    {
+        if (cellList.Count < 2) return;
+
+        // calculate only other planets gravity!
+        foreach (var cell in cellList) {
+            var resForce = Vector3.zero;
+            var cellRb = cell.GetComponent<Rigidbody>();
+            foreach (var testCell in cellList)
+            {
+                if (testCell == cell) continue;
+
+                var testRb = testCell.GetComponent<Rigidbody>();
+
+                var dir = testCell.gameObject.transform.position - cell.transform.position; // get the force direction
+                var dist2 = dir.sqrMagnitude; // get the squared distance
+
+                // calculate the force intensity using Newton's law
+                var gForce = G * cellRb.mass * testRb.mass / dist2;
+                resForce += gForce * dir.normalized; // accumulate in the resulting force variable
+            }
+            
+
+            if (cellRb.position.y < 0.2)
+            {
+                cellRb.AddForce(resForce); // apply the resulting gravity force
+                
+            }
+            else
+            {
+                cellRb.AddForce(new Vector3(0, -5, 0));
+            }
+        }
+    }
+}
